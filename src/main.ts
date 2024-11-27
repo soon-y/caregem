8daf799163bb74ba257ad0446b0b56f23f2f4a1b
Soonyoung Park <soon.syp@gmail.com>
2024-11-27 11:52:09 +0100

    add icons

--- src/main.ts
+++ src/main.ts
@@ -5,18 +5,18 @@ import App from './App.vue'
 import router from './router'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faCalendarDay } from '@fortawesome/free-solid-svg-icons'
-import { faPills } from '@fortawesome/free-solid-svg-icons'
-import { faChartColumn } from '@fortawesome/free-solid-svg-icons'
-import { faHeartPulse } from '@fortawesome/free-solid-svg-icons'
-import { faBed } from '@fortawesome/free-solid-svg-icons'
-import { faShoePrints } from '@fortawesome/free-solid-svg-icons'
-import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
-
-
+import { 
+  faCalendarDay,
+  faPills,
+  faChartColumn,
+  faHeartPulse,
+  faBed,
+  faShoePrints,
+  faPersonWalkingDashedLineArrowRight 
+} from '@fortawesome/free-solid-svg-icons'
 
 /* add each imported icon to the library */
-library.add(faCalendarDay,faPills,faChartColumn,faHeartPulse,faBed,faShoePrints,faLocationDot)
+library.add(faCalendarDay,faPills,faChartColumn,faHeartPulse,faBed,faShoePrints,faPersonWalkingDashedLineArrowRight)
 
 const app = createApp(App)
 
@@ -25,9 +25,3 @@ app.use(router)
 app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app')
-
-
-//<font-awesome-icon icon="fa-solid fa-heart-pulse" />
-//<font-awesome-icon icon="fa-solid fa-shoe-prints" />
-//<font-awesome-icon icon="fa-solid fa-location-dot" />
-//<font-awesome-icon icon="fa-solid fa-bed" />
