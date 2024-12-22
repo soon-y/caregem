5df628d29ee0ef055e463d8e9d055e791d004a0e
Soonyoung Park <soon.syp@gmail.com>
2024-12-23 00:07:51 +0100

    change top title size

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -2,7 +2,7 @@
   <div class="container-full">
     <div class="fixed">
         <span class="closeIcon topIcon"><slot name="close"></slot></span>
-        <span class="top-title" :style="{fontSize: '1.2rem', marginTop:'0.7rem'}"><slot name="name"></slot></span>
+        <span class="top-title" :style="{marginTop:'1rem'}"><slot name="name"></slot></span>
     </div>
 
     <div class="scroll-wrapper-full">
@@ -34,7 +34,7 @@
         <span :style="{ color: 'var(--white-lila-border)' }"><slot name="empty"></slot></span>
       </div>
 
-      <button class="button"  :style="{ backgroundColor: 'var(--white-lila)', color: 'var(--main-lila-hell)' }">show All data</button>
+      <button class="button"  :style="{ margin: '1rem 0', backgroundColor: 'var(--white-lila)', color: 'var(--main-lila-hell)' }">show All data</button>
       <button class="button"  :style="{ margin: '0 0 5rem 0', backgroundColor: 'var(--white-lila)', color: 'var(--main-lila-hell)' }">Delete Medication</button>  
     
       </div>
