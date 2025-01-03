ff279f3f79d498e83ef5a9425c7d16c5e1c2bf43
Soonyoung Park <soon.syp@gmail.com>
2025-01-03 19:14:46 +0100

    update device info

--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import Box from '../components/Box.vue'
 
 const now = new Date()
 const battery = ref<number>(100)
@@ -23,66 +22,94 @@ calcBattery()
       <div>
         <img src="/portrait.jpg" class="account-img"/>
       </div>
-
-    <div>
-      <h1 class="center" :style="{ padding: '1rem 0' }">John Deo</h1>
-    </div>
-
-    <div class="card-wrapper">
-      <font-awesome-icon icon="person-arrow-up-from-line" class="icon" />
-      <span :style="{ color: 'var(--white-lila-dunkel)' }">Height</span>
-      <font-awesome-icon icon="chevron-right" class="box-arrowRight"/>
       <div>
-        <span class="value">180</span>
-        <span class="unit">cm</span>
+        <h1 class="center" :style="{ padding: '1rem 0' }">John Deo</h1>
       </div>
-    </div>
 
-    <div class="card-wrapper">
-      <font-awesome-icon icon="weight-scale" class="icon" />
-      <span :style="{ color: 'var(--white-lila-dunkel)' }">Weight</span>
-      <font-awesome-icon icon="chevron-right" class="box-arrowRight"/>
+      <div class="card-wrapper">
+        <font-awesome-icon icon="person-arrow-up-from-line" class="icon" />
+        <span :style="{ color: 'var(--white-lila-dunkel)' }">Height</span>
+        <font-awesome-icon icon="chevron-right" class="box-arrowRight"/>
+        <div>
+          <span class="value">180</span>
+          <span class="unit">cm</span>
+        </div>
+      </div>
+
+      <div class="card-wrapper">
+        <font-awesome-icon icon="weight-scale" class="icon" />
+        <span :style="{ color: 'var(--white-lila-dunkel)' }">Weight</span>
+        <font-awesome-icon icon="chevron-right" class="box-arrowRight"/>
+        <div>
+          <span class="value">68</span>
+          <span class="unit">kg</span>
+        </div>
+      </div>
+
+      <p class="input-subtitle margin-top"> Device </p>
       <div>
-        <span class="value">68</span>
-        <span class="unit">kg</span>
+        <div class="table-wrapper" :style="{ padding: '.6rem 1rem 0.1rem 1rem' }">
+          <div :style="{ textAlign: 'right', verticalAlign: 'top'}">
+            <div>
+              <div v-if="battery > 75"> 
+                <font-awesome-icon icon="battery-full" class="icon" />
+                <span :style="{ color: 'var(--white-lila-dunkel)' }">{{battery}}%</span>
+              </div>
+              <div v-if="battery <= 75 && battery > 50"> 
+                <font-awesome-icon icon="fa-solid fa-battery-three-quarters" class="icon" />
+                <span :style="{ color: 'var(--white-lila-dunkel)' }">{{battery}}%</span>
+              </div>
+              <div v-if="battery <= 50 && battery > 25"> 
+                <font-awesome-icon icon="fa-solid fa-battery-half" class="icon" />
+                <span :style="{ color: 'var(--white-lila-dunkel)' }">{{battery}}%</span>
+              </div>
+              <div v-if="battery <= 25 && battery > 0"> 
+                <font-awesome-icon icon="fa-solid fa-battery-quarter" class="icon" />
+                <span :style="{ color: 'var(--white-lila-dunkel)' }">{{battery}}%</span>
+              </div>
+            </div>
+          </div>
+          <div :style="{ textAlign: 'center' }">
+            <img src="/watch.png" class="watch"/>
+          </div>
+          <div :style="{ textAlign: 'center' }">
+            <span class="box-name" :style="{ textAlign: 'center' }">Caregem Pro </span>
+          </div>
+          <table :style="{  margin: '1rem auto', borderCollapse: 'collapse' }">
+            <tbody>
+              <tr>
+                <td class="td-height">Syncing <font-awesome-icon icon="rotate" class="icon"/></td>
+                <td class="td-height align-right" :style="{ color: 'var(--white-lila-dunkel)' }">
+                  Updated 1 min ago
+                </td>
+              </tr>
+              <tr>
+                <td class="td-height">Serial Number</td>
+                <td class="td-height align-right">KR21354324</td>
+              </tr>
+              <tr>
+                <td class="td-height">Since</td>
+                <td class="td-height align-right">1 January 2023</td>
+              </tr>
+              <tr>
+                <td class="td-height">Status</td>
+                <td class="td-height align-right">{{ status }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
 
-    <p class="input-subtitle margin-top"> Device </p>
-      <Box>
-        <template v-slot:image>
-          <img src="/watch.png" />
-        </template>
-        <template v-slot:name> Caregem Pro </template>
-        <template v-slot:sync>
-          Syncing <font-awesome-icon icon="rotate" class="icon"/>
-        </template>
-        <template v-slot:update>
-          Updated 1 min ago
-        </template>
-        <template v-slot:number>KR21354324 </template>
-        <template v-slot:schedule>1 January 2023 </template>
-        <template v-slot:status> {{ status }} </template>
-        <template v-slot:battery v-if="battery > 75"> 
-          <font-awesome-icon icon="battery-full" class="icon" />
-          <span :style="{ color: 'var(--white-lila-dunkel)' }">{{battery}}%</span>
-        </template>
-         <template v-slot:battery v-if="battery <= 75 && battery > 50"> 
-          <font-awesome-icon icon="fa-solid fa-battery-three-quarters" class="icon" />
-          <span :style="{ color: 'var(--white-lila-dunkel)' }">{{battery}}%</span>
-        </template>
-        <template v-slot:battery v-if="battery <= 50 && battery > 25"> 
-          <font-awesome-icon icon="fa-solid fa-battery-half" class="icon" />
-          <span :style="{ color: 'var(--white-lila-dunkel)' }">{{battery}}%</span>
-        </template>
-        <template v-slot:battery v-if="battery <= 25 && battery > 0"> 
-          <font-awesome-icon icon="fa-solid fa-battery-quarter" class="icon" />
-          <span :style="{ color: 'var(--white-lila-dunkel)' }">{{battery}}%</span>
-        </template>
-      </Box>
+      <button class="button" :style="{ 
+        marginBottom: '0rem', backgroundColor: 'var(--white-lila)', color: 'var(--white-lila-dunkel)',
+        borderRadius: '0.8rem 0.8rem 0 0', borderBottom: '2px solid white'
+        }">Add Account</button>
+      <button class="button" :style="{ 
+        marginTop: '0rem', backgroundColor: 'var(--white-lila)', color: 'var(--white-lila-dunkel)',
+        borderRadius: '0 0 0.8rem 0.8rem'
+        }">Switch Account</button>
+    </div>
   </div>
-</div>
-
 </template>
 
 <style scope>
@@ -94,6 +121,11 @@ calcBattery()
     margin: auto;
     margin-top: 1.5rem;
     display: block;
+  }
+
+  .td-height {
+  height: 1.8rem;
+  line-height: 1;
   }
 
   .icon{
@@ -108,6 +140,11 @@ calcBattery()
     border-radius: 0.8rem;
     margin-bottom: 1rem;
     padding: 1rem 0 0.4rem 1rem;
+  }
+
+  .watch{
+    height: 10rem;
+    margin: auto;
   }
 
   .value{
